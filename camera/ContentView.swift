1eb854bec9e20582ad357a41765097a7ba2f3024
louis-kaiser <louis.kaiser0203@icloud.com>
2024-02-15 10:59:35 +0100

    Picker

--- camera/ContentView.swift
+++ camera/ContentView.swift
@@ -8,21 +8,8 @@
 import SwiftUI
 import AVFoundation
 
-class GlobalModel: ObservableObject {
-    @Published var selectedCamera: AVCaptureDevice? = AVCaptureDevice.default(for: .video)
-}
-
 struct ContentView: View {
-    
-    @EnvironmentObject private var globalModel: GlobalModel
-    
     var body: some View {
-        VStack {
-            CameraView()
-                .environmentObject(globalModel)
-                .frame(width: 400, height: 300)
-            CameraPicker()
-                .environmentObject(globalModel)
-        }
+        CameraPicker()
     }
 }
--- camera/cameraApp.swift
+++ camera/cameraApp.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 @main
 struct cameraApp: App {
-    
-    @StateObject var globalModel = GlobalModel()
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(globalModel)
         }
     }
 }
